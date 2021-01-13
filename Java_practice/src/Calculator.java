2ae97b504dc375e99c542366a0ec57736823e931
swwho96 <tjtmddn24@naver.com>
2021-01-13 23:28:37 +0900

    The Second Commit

--- Java_practice/src/Calculator.java
+++ Java_practice/src/Calculator.java
@@ -1,17 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
 public class Calculator extends JFrame{
-	public Calculator() {
-		setTitle("계산기");
-		setDefaultCloseOperation(Calculator.EXIT_ON_CLOSE);
-		
+	
+	
+	public Calculator() {		
+		JFrame frame = new JFrame();
 		JPanel display = new JPanel();
 		JPanel calculate = new JPanel();
 		
-		display.add(new JTextField(30));
+		frame.setTitle("계산기");
+		frame.setDefaultCloseOperation(Calculator.EXIT_ON_CLOSE);
 		
+		display.setLayout(null);
+		display.setSize(50, 50);
+		display.setBackground(Color.GREEN);
+		display.add(new JTextArea(2,30));
+		display.add(new JTextArea(2,30));
 		
 		calculate.setLayout(new GridLayout(6,4));
+		calculate.setBackground(Color.GREEN);
 		calculate.add(new JButton("%"));
 		calculate.add(new JButton("CE"));
 		calculate.add(new JButton("C"));
@@ -25,12 +32,27 @@ public class Calculator extends JFrame{
 		calculate.add(new JButton("9"));
 		calculate.add(new JButton("*"));
 		calculate.add(new JButton("4"));
+		calculate.add(new JButton("5"));
+		calculate.add(new JButton("6"));
+		calculate.add(new JButton("-"));
+		calculate.add(new JButton("1"));
+		calculate.add(new JButton("2"));
+		calculate.add(new JButton("3"));
+		calculate.add(new JButton("+"));
+		calculate.add(new JButton("+/-"));
+		calculate.add(new JButton("0"));
+		calculate.add(new JButton("."));
+		calculate.add(new JButton("="));
 
-		getContentPane().add(display, BorderLayout.CENTER);
-		getContentPane().add(calculate, BorderLayout.SOUTH);
-		setSize(1000,700);
-		setVisible(true);
+		frame.setLayout(new GridLayout(3,1));
+		frame.setSize(1000,1000);
+		frame.setVisible(true);
+		frame.add(display);
+		frame.add(calculate);
 	}
+	
+	
+	//계산기 실행
 	public static void main(String[] str) {
 		new Calculator();
 	}
